534466fdea63b1d8254d80533fc0a00d21fdc474
Emil Öhlund <51457329+emilohlund-git@users.noreply.github.com>
2020-06-16 20:54:18 +0200

    Delete env.py

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_UeQ6rAh3YOZYCwtAZYWpwn6X00RFwcDAmn")
-os.environ.setdefault("STRIPE_SECRET", "sk_test_Ud5dmX7dmeU2Ocrl1zNqfabJ006egWonBa")
-os.environ.setdefault("DATABASE_URL", "postgres://mreodrkoaewwab:9b471ea021d8ee1f502324b3886cc1e3110c5d939bb6e54d087ebeeb26d32a1b@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/d3ftt2549rh0rg")
-os.environ.setdefault("SECRET_KEY", "a@%lf8tpa99n_j42j30e2v)^6l7crgx09ttu81broul+oejr#u")
-os.environ.setdefault('AWS_ACCESS_KEY_ID', 'AKIA2AIR22C5ICEFZ5N2')
-os.environ.setdefault('AWS_SECRET_ACCESS_KEY', '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')
-os.environ.setdefault('EMAIL_PASSWORD', 'Emil1234')
